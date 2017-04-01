c441308c19157b351cdd60a24e571a74af5fcaa8
Zachary Christoff <zkristov@gmail.com>
2017-03-31 19:26:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Caffe
+
+A modern web mapping application. Built with [Angular](https://angular.io/) and 
+[ESRI](https://developers.arcgis.com/).
---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,25 @@
+import { AppComponent } from './app.component';
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+describe('AppComponent', function () {
+  let comp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    comp = fixture.componentInstance;
+  });
+
+  it('should create component', () => expect(comp).toBeDefined() );
+
+});
